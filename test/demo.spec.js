072ad88f6ba86bf7d89fae5851e50aeade505299
Sathishag2706 <131006505+Sathishag2706@users.noreply.github.com>
2026-05-01 23:02:33 +0530

    your message

--- test/demo.spec.js
+++ test/demo.spec.js
@@ -21,7 +21,8 @@ await childframe.locator("li a[href*='lifetime-access']:visible").click();
 const text = await childframe.locator(".text h2").textContent();
 console.log(text.split(" ")[0]);
 ////ssssss///
-
+//// created da//
+////sssss///
 
 
 
